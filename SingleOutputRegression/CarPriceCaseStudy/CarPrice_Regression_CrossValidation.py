eb10a093f227f65df6f86a743441c3fc3146b0ec
Caio Medeiros <caio.s.medeiros@outlook.com>
2024-02-20 09:02:02 -0300

    ,

--- SingleOutputRegression/CarPriceCaseStudy/CarPrice_Regression_CrossValidation.py
+++ SingleOutputRegression/CarPriceCaseStudy/CarPrice_Regression_CrossValidation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from gc import collect
 
 base = pd.read_csv("autos.csv", encoding='ISO-8859-1')
 
@@ -15,7 +16,6 @@ base = base.drop('abtest', axis=1)
 base = base.drop('monthOfRegistration', axis=1)
 base = base.drop('powerPS', axis=1)
 base = base.drop('brand', axis=1)
-base = base.drop('vehicleType', axis=1)
 
 base = base.loc[base['price'] > 400]
 base = base.loc[base['price'] < 350000]
@@ -32,12 +32,12 @@ X = base.iloc[:, 1:9].values
 y = base.iloc[:, 0].values
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder   
-for i in [1, 2, 4, 5]:
+for i in [ 0, 2, 3, 5, 6]:
     X[:, i] = LabelEncoder().fit_transform(X[:, i])
 
 from sklearn.compose import ColumnTransformer
 
-transformer = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(), [1, 2, 4, 5])], remainder='passthrough')
+transformer = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(), [0, 2, 3, 5, 6])], remainder='passthrough')
 X = transformer.fit_transform(X).toarray()
 
 y = np.reshape(y, (-1, 1))
@@ -49,16 +49,27 @@ X = scaler.fit_transform(X)
 y = scaler.fit_transform(y)
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, InputLayer
 from keras.optimizers import Adam
 
 def network():
-    model = Sequential()
-    model.add(Dense(units=132, activation='relu', input_dim=264))
-    model.add(Dense(units=132, activation='relu'))
-    model.add(Dense(units=1, activation='linear'))
-    model.compile(optimizer=Adam(learning_rate=0.0001), loss='mean_squared_logarithmic_error', metrics=['mean_squared_logarithmic_error'])
-    return model
+    regressor = Sequential()
+    regressor.add(InputLayer(input_shape=(272, )))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+    regressor.add(Dense(units=272, activation='relu'))
+
+    regressor.add(Dense(units=1, activation='linear'))
+    regressor.compile(optimizer=Adam(learning_rate=0.00001), loss='mean_squared_logarithmic_error')
+    
+    return regressor
 
 from keras.wrappers.scikit_learn import KerasRegressor
 
@@ -67,7 +78,6 @@ regressor = KerasRegressor(build_fn=network, epochs=100, batch_size=64)
 from sklearn.model_selection import cross_val_score
 
 results = cross_val_score(estimator=regressor, X=X, y=y, cv=10, scoring='r2')
-results = results[-4]
 
 mean = results.mean()
 
