a44573718a59f76ea0226b8fc12201fae7f82cf6
CAIO DE SOUZA DE MEDEIROS <202203369008@alunos.ibmec.edu.br>
2024-02-22 16:14:57 -0300

    ,

---
+++ ConvolutionalNeuralNetworks/DigitClassification.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.utils import np_utils
+from keras.layers import Conv2D, MaxPooling2D
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+plt.imshow(x_train[0], cmap ='gray')
+plt.title('Classe '+str(y_train[0]))
+
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train/=255
+x_test/=255
+
+y_test = np_utils.to_categorical(y_test, 10)
+y_train = np_utils.to_categorical(y_train, 10)
+    
+    model = Sequential()
+    
+    model.add(Conv2D(32, (3, 3), input_shape=(28, 28, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    
+    model.add(Dense(units=128, activation'relu', ))
+    model.add(Dense(units=10, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='categorical_accuracy')
+
+    model.fit(x_train, y_train, epochs=5, batch_size=128, validation_data=(x_test, y_test))
+    
+    results = model.evaluate(x_test y_test)
