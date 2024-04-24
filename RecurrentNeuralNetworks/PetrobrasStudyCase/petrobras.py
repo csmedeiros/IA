86e64b080c8a44983cb859a29d48df72424e2421
csmedeiros <caio.s.medeiros@outlook.com>
2024-04-23 22:33:19 -0300

    update

--- RecurrentNeuralNetworks/PetrobrasStudyCase/petrobras.py
+++ RecurrentNeuralNetworks/PetrobrasStudyCase/petrobras.py
@@ -3,70 +3,71 @@ import numpy as np
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, LSTM
 from sklearn.preprocessing import MinMaxScaler
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
 base = pd.read_csv("petr4_treinamento.csv")
 base = base.dropna()
-train = base.iloc[:, 1:7].values
+train = base.iloc[:, 1:2].values
+high = base.iloc[:, 2:3].values
 
-scaler = MinMaxScaler()
-train = scaler.fit_transform(train)
+feature_scaler = MinMaxScaler()
+
+open_ = feature_scaler.fit_transform(train)
+high_ = feature_scaler.fit_transform(high)
 
 x_train = []
-y_train = []
+y_open = []
+y_high = []
 
 for i in range(90, 1242):
-    x_train.append(train[i-90:i, :])
-    y_train.append(train[i, 0])
-x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train.append(open_[i-90:i, 0])
+    y_open.append(open_[i, 0])
+    y_high.append(high_[i, 0])
+x_train, y_open, y_high = np.array(x_train), np.array(y_open), np.array(y_high)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+y_train = np.column_stack((y_open, y_high))
 
 regressor = Sequential()
-regressor.add(LSTM(units=100, input_shape=(x_train.shape[1], 6), return_sequences=True))
+regressor.add(LSTM(units=100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 regressor.add(Dropout(0.3))
-regressor.add(LSTM(units=100, input_shape=(x_train.shape[1], 6), return_sequences=True))
+regressor.add(LSTM(units=100, return_sequences=True))
 regressor.add(Dropout(0.3))
-regressor.add(LSTM(units=100, input_shape=(x_train.shape[1], 6), return_sequences=True))
+regressor.add(LSTM(units=100, return_sequences=False))
 regressor.add(Dropout(0.3))
-regressor.add(LSTM(units=100, input_shape=(x_train.shape[1], 6), return_sequences=False))
-regressor.add(Dropout(0.3))
+regressor.add(Dense(units=2, activation='linear'))
 
-regressor.add(Dense(units=1, activation="linear"))
-regressor.compile(optimizer="rmsprop", loss="mean_squared_error", metrics=["mean_absolute_error"])
+regressor.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=["mean_absolute_error"])
 
-es = EarlyStopping(monitor="loss", min_delta=1e-10, patience=10, verbose=1)
-rlr = ReduceLROnPlateau(monitor="loss", factor=0.2, patience=5, verbose=1)
-mcp = ModelCheckpoint("best.h5", monitor="loss", verbose=1, save_best_only=True, mode="min")
-callbacks_list = [es, rlr, mcp]
+callbacks_list = [ModelCheckpoint('best.h5', monitor='loss', verbose=1, save_best_only=True), EarlyStopping(monitor='loss', min_delta=1e-10, patience=10, verbose=1, mode='min'), ReduceLROnPlateau(monitor='loss', factor=0.2, patience=5, verbose=1, mode='min')]
 
-hist = regressor.fit(x_train, y_train, batch_size=32, epochs=100, callbacks=callbacks_list)
+hist = regressor.fit(x_train, y_train, epochs=100,  batch_size=32, callbacks=callbacks_list)
 
 test = pd.read_csv("petr4_teste.csv")
-y_test = test.iloc[:, 1:2].values
-
-complete = pd.concat((base, test))
-complete = complete.drop("Date", axis=1)
-
-test_inputs = complete[len(complete) - len(test) - 90:]
-test_inputs = scaler.transform(test_inputs)
+open_test = test.iloc[:, 1:2].values
+high_test = test.iloc[:, 2:3].values
+complete_base = pd.concat((base["Open"], test["Open"]), axis=0)
+inputs = complete_base[len(complete_base) - len(test) - 90:].values
+inputs = inputs.reshape(-1, 1)
+inputs = feature_scaler.transform(inputs)
 
 x_test = []
-for i in range(90, 112):
-    x_test.append(test_inputs[i-90:i, :])
-x_test = np.array(x_test)
 
-preds_scaler = MinMaxScaler()
-preds_scaler.fit_transform(base.iloc[:, 1:2].values)
+for i in range(90, 112):
+    x_test.append(inputs[i-90:i, 0])
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 best = load_model("best.h5")
 preds = best.predict(x_test)
-
-preds = preds_scaler.inverse_transform(preds)
+preds = feature_scaler.inverse_transform(preds)
 
 import matplotlib.pyplot as plt
 
-plt.plot(preds, color="blue", label="Predictions")
-plt.plot(test["Open"], color="red", label='Actual prices')
+plt.plot(test.iloc[:, 1:2].values, color='red', label="Open Price")
+plt.plot(test.iloc[:, 2:3].values, color='black', label="High Price")
+plt.plot(preds[:, 0], color='blue', label="Open Predictions")
+plt.plot(preds[:, 1], color='orange', label="High Predictions")
 plt.xlabel("Time")
-plt.ylabel("Prices")
+plt.ylabel("Yahoo Prices")
 plt.legend()
 plt.show()
